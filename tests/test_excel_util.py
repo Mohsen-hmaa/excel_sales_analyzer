181f14f1e578cb35b22a6e6f6cb47053594a821f
mohsen <mohsen_hmaa@yahoo.com>
2026-07-22 22:42:10 +0330

    Add test for report generation

--- tests/test_excel_util.py
+++ tests/test_excel_util.py
@@ -1,5 +1,5 @@
-from openpyxl import Workbook
-from excel_util import calculate_file_total
+from openpyxl import Workbook, load_workbook
+from excel_util import calculate_file_total, create_report
 def test_calculate_file_total(tmp_path):
     file_path = tmp_path / "test_sales.xlsx"
     workbook = Workbook()
@@ -20,3 +20,16 @@ def test_calculate_file_total(tmp_path):
     result = calculate_file_total(file_path)
 
     assert result == 350
+def test_create_report(tmp_path):
+    report={"file1.xlsx":350
+            ,"file2.xlsx":650
+    }
+    output_file=tmp_path / "report.xlsx"
+    create_report(report, output_file)
+    assert output_file.exists()
+    workbook= load_workbook(output_file)
+    sheet = workbook.active
+    assert sheet["A2"].value == "file1.xlsx"
+    assert sheet["B2"].value == 350
+    assert sheet["A4"].value == "TOTAL"
+    assert sheet["B4"].value == 1000
